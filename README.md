8c16114f3e8ee92001ae8dd97132a3f4f4681879
WilliamChow <william@mbp.local>
2017-09-08 23:43:52 +0800

    solve problem 14

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 |7|[Reverse Integer](https://leetcode.com/problems/reverse-integer/description/)| [JavaScript](./algorithms/reverseInteger.js)|Easy|
 |9|[Palindrome Number](https://leetcode.com/problems/palindrome-number/description/)| [JavaScript](./algorithms/palindromeNumber.js)|Easy|
 |13|[Roman to Integer](https://leetcode.com/problems/roman-to-integer/description/)| [JavaScript](./algorithms/romanToInteger.js)|Easy|
+|14|[Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/description/)|[JavaScript](./algorithms/longestCommonPrefix.js)|Easy|
---
+++ algorithms/longestCommonPrefix.js
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/add-two-numbers/description/
+// Author : zhouweilin
+// Date   : 2017-09-08
+// DS     :
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    var cp = "";
+    if(strs.length === 0) return cp;            //空串
+    for(var i = 0; i < strs[0].length; i++){    //i遍历第一个串
+        var ch = strs[0].charAt(i);
+        for(var j = 1; j < strs.length; j++){   //j遍历整个strs数组
+            if(ch !== strs[j].charAt(i))
+                return cp;
+        }
+        cp += ch;
+    }
+    return cp;
+};
