5d194f4da72a4e8c0bf9695c87c1efd5670c6bb3
Si3ver <zhou_weilin@outlook.com>
2017-09-11 15:39:59 +0800

    solve problem 20

--- algorithms/validParentheses.js
+++ algorithms/validParentheses.js
@@ -1,4 +1,27 @@
 // Source : https://leetcode.com/problems/non-decreasing-array/description/
 // Author : zhouweilin
 // Date   : 2017-09-11
-// DS     : 
+// DS     : stack
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    var c = new Array();    //use as a stack
+    var dic = {             //dic
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    };
+    for(var i = 0; i < s.length; i++){
+        if(c[i] === "undefined" || dic[c[c.length-1]] !== s[i]){
+            c.push(s[i]);
+        }else if(dic[c[c.length-1]] === s[i]){
+            c.pop();
+        }else{
+            return false;
+        }
+    }
+    return c.length === 0;
+};
