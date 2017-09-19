d601c640c5d01be530f94c1efcd59fe5e73a8e23
Si3ver <zhou_weilin@outlook.com>
2017-09-19 23:20:59 +0800

    add some 注释 for problem 12

--- algorithms/_012_intToRoman.js
+++ algorithms/_012_intToRoman.js
@@ -18,9 +18,9 @@ var intToRoman = function(num) {
     //     'D': 500,
     //     'M': 1000
     // };
-    var I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
-        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
-        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
-        M = ["", "M", "MM", "MMM"];
+    var I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],   // 0~9
+        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   //00~90
+        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],   //000~900
+        M = ["", "M", "MM", "MMM"]; //0000~3000
     return M[Math.floor(num/1000)] + C[Math.floor(num%1000/100)] + X[Math.floor(num%100/10)] + I[Math.floor(num%10)];
 };
